9235fe51072e3edd877b8de7b1aaafc4103a4aff
studiopanko <50788590+studiopanko@users.noreply.github.com>
2025-03-13 19:14:05 +0800

    1st build push from unity

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "panko-3LRPS-1.0";
+const contentToCache = [
+    "Build/3LRPSweb.loader.js",
+    "Build/3LRPSweb.framework.js.br",
+    "Build/3LRPSweb.data.br",
+    "Build/3LRPSweb.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
